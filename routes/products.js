1aaa50d4f4db1e524c8fff90f2be1544b7c59b8d
RoshanAnsy <roshanansy123@gmail.com>
2023-10-13 00:19:05 +0530

    minPrice-maxPrice

--- routes/products.js
+++ routes/products.js
@@ -2,8 +2,33 @@ const express = require("express");
 const router = express.Router();
 const Products = require("../Products.json");
 
-router.get("/", (req, res) => {
-  res.send(Products);
+router.get("/", async(req, res) => {
+  try{
+
+  
+    const {minPrice,maxPrice}=req.body
+    let ans=[]
+    for(let i=0;i<Products.length; i++){
+      if(minPrice < Products[i].price && Products[i].price < maxPrice){
+        ans.push(Products[i]);
+      }
+      
+        
+    }
+    res.status(200).json({
+      message:`All product are here bt ${minPrice} and ${maxPrice} `,
+      success:true,
+      ans,
+    })
+  }
+  catch(error){
+    res.status(400).json({
+      message:"something error in finding the product bt these price",
+      success:false,
+      error:error.message,
+    })
+  }
+  
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 8000;
 const productsRouter = require("./routes/products");
+const bodyParser = require("body-parser");
 
 app.get("/", (req, res) => res.send("Hello World!"));
+app.use(express.json());
+app.use(bodyParser.json());
 app.use("/products", productsRouter);
 
 app.listen(port, () => console.log(`http://localhost:${port}`));
