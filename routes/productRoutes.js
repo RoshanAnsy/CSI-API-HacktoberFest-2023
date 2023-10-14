1a7746418a607751fdb6f20e1a6ac7616d5bc0c3
RoshanAnsy <roshanansy123@gmail.com>
2023-10-14 11:40:38 +0530

    added good Folder structure

---
+++ routes/productRoutes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const {getAllProduct}=require("../controller/products")
+
+router.get("/", getAllProduct);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 8000;
-const productsRouter = require("./routes/products");
+const productsRouter = require("./routes/productRoutes")
 const bodyParser = require("body-parser");
 
 app.get("/", (req, res) => res.send("Hello World!"));
